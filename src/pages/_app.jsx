ea16cd131ac1d1a6dcecf281cd361df6d722f110
Sergio Ferrari <ferrari8986@gmail.com>
2024-07-19 23:35:22 -0300

    wiped config.json fixed _app.jsx leftovers

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types';
 // Styles / CSS
 import '../styles/globals.css'; // Our global stylesheet
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap css
-
 import SEO from '../components/SEO';
-import config from '../../config.json';
+
+const PAGE_TITLE = 'SKVAD Community';
+const PAGE_SUB_TITLE = 'DayZ';
+const PAGE_DESCRIPTION = 'DayZ Leaderboard for SKVAD Community. Browse the top players, and display your own detailed player statistics.';
+const BRANDING_LOGO_FILE_NAME = 'logo.png';
+const BRANDING_THEME_COLOR = '#00FFFF';
+
 
 function MyApp ({
   Component,
@@ -15,13 +20,13 @@ function MyApp ({
   return (<>
     {/* Default Search Engine Optimization */}
     <SEO
-      title={config.PAGE_TITLE}
-      subTitle={config.PAGE_SUB_TITLE}
-      description={config.PAGE_DESCRIPTION}
-      imageURL={process.env.NEXT_PUBLIC_IMAGE_URL || `/${config.BRANDING_LOGO_FILE_NAME}`}
-      favicon={config.BRANDING_LOGO_FILE_NAME}
+      title={PAGE_TITLE}
+      subTitle={PAGE_SUB_TITLE}
+      description={PAGE_DESCRIPTION}
+      imageURL={process.env.NEXT_PUBLIC_IMAGE_URL || `/${BRANDING_LOGO_FILE_NAME}`}
+      favicon={BRANDING_LOGO_FILE_NAME}
       largeImg={process.env.NEXT_PUBLIC_LARGE_OG_IMAGE === 'true'}
-      color={config.BRANDING_THEME_COLOR}
+      color={BRANDING_THEME_COLOR}
     />
     <Component {...pageProps} />
   </>);
